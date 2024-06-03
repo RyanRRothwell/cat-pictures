53a5af11aca6e07933a94cc7f71a8662d0d57a60
Ryan R Rothwell <Ryan.Rothwell@live.ca>
2024-06-02 20:43:33 -0400

    added storage vars

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function loadImages() {
-    const response = await fetch('https://<YOUR-STORAGE-ACCOUNT>.blob.core.windows.net/<YOUR-CONTAINER>?restype=container&comp=list');
+    const response = await fetch('https://devrrrcatpictures.blob.core.windows.net/images?restype=container&comp=list');
     const data = await response.text();
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(data, "text/xml");
@@ -7,7 +7,7 @@ async function loadImages() {
     const gallery = document.getElementById('gallery');
     
     for (let i = 0; i < blobs.length; i++) {
-        const url = `https://<YOUR-STORAGE-ACCOUNT>.blob.core.windows.net/<YOUR-CONTAINER>/${blobs[i].getElementsByTagName("Name")[0].textContent}`;
+        const url = `https://devrrrcatpictures.blob.core.windows.net/images/${blobs[i].getElementsByTagName("Name")[0].textContent}`;
         const img = document.createElement('img');
         img.src = url;
         const container = document.createElement('div');
